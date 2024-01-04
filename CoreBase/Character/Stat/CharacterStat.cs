e3583d3c84258c3f0ab04a2111043e3fca022881
Juan Montealegre <juanmm990@gmail.com>
2024-01-04 18:45:09 -0500

    adjustments to stat class

---
+++ CoreBase/Character/Stat/CharacterStat.cs
@@ -0,0 +1,133 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+
+namespace CoreBase.Character.Stat;
+
+public class CharacterStat
+{
+    private bool isDirty = true;
+    private float _value;
+
+    public float BaseValue;
+
+    private readonly List<StatModifier> statModifiers;
+    public CharacterStat(float baseValue)
+    {
+        BaseValue = baseValue;
+        statModifiers = new List<StatModifier>();
+    }
+
+    public class StatModifier
+    {
+        public readonly float Value;
+        public readonly StatModType Type;
+        public readonly int Order;
+        public readonly object Source;
+
+        public StatModifier(float value, StatModType type, int order, object source)
+        {
+            Value = value;
+            Type = type;
+            Order = order;
+            Source = source;
+        }
+
+        public StatModifier(float value, StatModType type) : this(value, type, (int)type, null) { }
+
+        public StatModifier(float value, StatModType type, int order) : this(value, type, order, null) { }
+
+        public StatModifier(float value, StatModType type, object source) : this(value, type, (int)type, source) { }
+    }
+
+    public float Value
+    {
+        get
+        {
+            if (isDirty)
+            {
+                _value = CalculateFinalValue();
+                isDirty = false;
+            }
+            return _value;
+        }
+    }
+
+    public void AddModifier(StatModifier mod)
+    {
+        isDirty = true;
+        statModifiers.Add(mod);
+        statModifiers.Sort(CompareModifierOrder);
+    }
+
+    public bool RemoveModifier(StatModifier modifier)
+    {
+        isDirty = true;
+        return statModifiers.Remove(modifier);
+    }
+
+    private float CalculateFinalValue()
+    {
+        float finalValue = BaseValue;
+        float sumPercentAdd = 0; // This will hold the sum of our "PercentAdd" modifiers
+
+        for (int i = 0; i < statModifiers.Count; i++)
+        {
+            StatModifier mod = statModifiers[i];
+
+            if (mod.Type == StatModType.Flat)
+            {
+                finalValue += mod.Value;
+            }
+            else if (mod.Type == StatModType.PercentAdd) // When we encounter a "PercentAdd" modifier
+            {
+                sumPercentAdd += mod.Value; // Start adding together all modifiers of this type
+
+                // If we're at the end of the list OR the next modifer isn't of this type
+                if (i + 1 >= statModifiers.Count || statModifiers[i + 1].Type != StatModType.PercentAdd)
+                {
+                    finalValue *= 1 + sumPercentAdd; // Multiply the sum with the "finalValue", like we do for "PercentMult" modifiers
+                    sumPercentAdd = 0; // Reset the sum back to 0
+                }
+            }
+            else if (mod.Type == StatModType.PercentMult) // Percent renamed to PercentMult
+            {
+                finalValue *= 1 + mod.Value;
+            }
+        }
+
+        return (float)Math.Round(finalValue, 4);
+    }
+
+    public enum StatModType
+    {
+        Flat,
+        PercentAdd,
+        PercentMult
+    }
+
+    private int CompareModifierOrder(StatModifier a, StatModifier b)
+    {
+        if (a.Order < b.Order)
+            return -1;
+        else if (a.Order > b.Order)
+            return 1;
+        return 0; // if (a.Order == b.Order)
+    }
+
+    public bool RemoveAllModifiersFromSource(object source)
+    {
+        bool didRemove = false;
+
+        for (int i = statModifiers.Count - 1; i >= 0; i--)
+        {
+            if (statModifiers[i].Source == source)
+            {
+                isDirty = true;
+                didRemove = true;
+                statModifiers.RemoveAt(i);
+            }
+        }
+        return didRemove;
+    }
+}
